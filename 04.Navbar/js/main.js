e4b3ccf864005ec7847e21240e2c30ebf0d80c42
GUilherme <guilhermeclarodemorais@gmail.com>
2022-09-23 10:48:39 -0300

    Project

---
+++ 04.Navbar/js/main.js
@@ -0,0 +1,9 @@
+const hamburguer = document.querySelector(".mobile-menu");
+const hambText = document.querySelector(".menuMenu");
+
+
+hamburguer.addEventListener("click", () =>{
+    hamburguer.classList.toggle('active');
+    hambText.classList.toggle('active');
+    
+})
