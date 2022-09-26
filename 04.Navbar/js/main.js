dfa7a6a546d95aa06e7f6c6a64ff2fda9ec5a16a
GUilherme <guilhermeclarodemorais@gmail.com>
2022-09-26 11:25:48 -0300

    Changes

--- 04.Navbar/js/main.js
+++ 04.Navbar/js/main.js
@@ -5,5 +5,4 @@ const hambText = document.querySelector(".menuMenu");
 hamburguer.addEventListener("click", () =>{
     hamburguer.classList.toggle('active');
     hambText.classList.toggle('active');
-    
 })
